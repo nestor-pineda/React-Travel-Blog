d584f97d41d226c5c45e72097f934009ff557993
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-01-18 16:35:51 +0100

    Errors catched and printed in browser

--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,30 @@ import List from "./List";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
+    fetch("http://localhost:8000/blogss")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("No hemos podido encontrar esa data");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
         setLoading(false);
+        setError(null);
       })
       .catch((error) => {
-        console.log(error.message);
+        // setLoading(false);
+        setError(error.message);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {loading && <div>Loading...</div>}
       {blogs && <List blogs={blogs} title="All Blogs" />}
     </div>
