5bb251a4a3327c9000e44dc5c86dbbf02775ad17
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-01-24 17:18:57 +0100

    Reused fetch custom hook to get the setails information

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./CustomHooks/Fetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { apiData: blog, loading, error } = useFetch("http://localhost:8000/blogs/" + id);
   return (
-    <div>
-      <h1>Blog Details - {id}</h1>
+    <div className="blogDetails">
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by: {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
