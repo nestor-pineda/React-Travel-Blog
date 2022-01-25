92b984042fcf33d7780a95bdb07639ce6f940d8c
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-01-25 10:29:00 +0100

    New Blog Post form created using onChange function to triggfer the new state of the input fields

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
   return (
     <div className="create">
       <h2>Add a new Blog Post</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+        <label>Blog Content:</label>
+        <textarea required value={content} onChange={(e) => setContent(e.target.value)} />
+
+        <label>Blog Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Nestor">Nestor</option>
+          <option value="Raquel">Raquel</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+      <p>{title}</p>
+      <p>{content}</p>
+      <p>{author}</p>
     </div>
   );
 };
